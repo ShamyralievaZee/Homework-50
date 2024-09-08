f0c800758c266a30acedb71506a344c2791542c9
Shamyralieva Zeinep <zeinepshamyralieva01@gmail.com>
2024-09-09 05:04:33 +0600

    Finished adding the rest of components(movies) and fixed some issues and index-1.html as well as tweaked some styles in style.css.

--- task-01/app.js
+++ task-01/app.js
@@ -1,16 +1,21 @@
-let noPoster = '';
+let noPoster = 'https://static.displate.com/857x1200/displate/2022-04-15/7422bfe15b3ea7b5933dffd896e9c7f9_46003a1b7353dc7b5a02949bd074432a.jpg';
 const Movie = ({poster,name,year}) =>{
+    console.log(poster);
     return (
-        <div className='movieWrap'>
-            <img width="300" src={poster === undefined ? noPoster : poster} alt="name"/>
-            <h2>{name}</h2>
-            <p>Year: {year}</p>
-        </div>
+        <a href="#" className='movieWrap'>
+                <img width="300px" height="427px" src={!(poster)? noPoster : poster} alt={name}/>
+                <h2 className='movieName'>{name}</h2>
+                <p className='movieYear'>Year: {year}</p>
+        </a>
     );
 };
 
 const container = document.getElementById('root');
-const movies = [{poster:'https://m.media-amazon.com/images/M/MV5BMzQxNjM5NzkxNV5BMl5BanBnXkFtZTcwMzg5NDMwMg@@._V1_.jpg', name:'Coraline',year:2009}];
+const movies = [
+    {poster:'https://m.media-amazon.com/images/I/91XY1nLj1XL._AC_UF1000,1000_QL80_.jpg', name:'The girl from the other side',year: 2019},
+    {poster:'https://m.media-amazon.com/images/M/MV5BYzhjNzI0NWEtMzEyYy00OGViLThhMjctMWQxNGQ3Y2IzOTY4XkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_.jpg', name:'Over the garden wall', year:2014},
+    {poster:'https://upload.wikimedia.org/wikipedia/en/3/36/Coraline_poster.jpg', name:'Coraline, world of Nightmares',year:2009},
+];
 
 const content = movies.map((movie,index) => {
     return(
