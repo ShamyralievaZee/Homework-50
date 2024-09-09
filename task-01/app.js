ac5a86ab0ad57027458a55a8f367edd7ab258a7f
Shamyralieva Zeinep <zeinepshamyralieva01@gmail.com>
2024-09-09 14:30:25 +0600

    Removed unneeded console.log

--- task-01/app.js
+++ task-01/app.js
@@ -1,6 +1,5 @@
 let noPoster = 'https://static.displate.com/857x1200/displate/2022-04-15/7422bfe15b3ea7b5933dffd896e9c7f9_46003a1b7353dc7b5a02949bd074432a.jpg';
 const Movie = ({poster,name,year}) =>{
-    console.log(poster);
     return (
         <a href="#" className='movieWrap'>
                 <img width="300px" height="427px" src={!(poster)? noPoster : poster} alt={name}/>
